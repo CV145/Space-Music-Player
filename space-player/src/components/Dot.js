92b8e0768d6a7e3201b6b89e7adf0fdcc99f31c4
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2023-11-04 16:36:36 -0500

    Update to dot animations (they move freely around)

--- space-player/src/components/Dot.js
+++ space-player/src/components/Dot.js
@@ -1,32 +1,50 @@
 // Dot.js
-import React, {useEffect, useLayoutEffect, useRef} from 'react';
-import '../styles/Dot.css'; // Import the CSS for styling
+import React, { useLayoutEffect, useRef } from 'react';
+import '../styles/Dot.css';
 import gsap from 'gsap';
 
 function Dot({ x, y }) {
-    const dotRef = useRef();
+  const dotRef = useRef();
 
-    //Fires after all DOM mutations
-    useLayoutEffect(() => {
-        let ctx = gsap.context(() => {
-            gsap.to(dotRef.current, {
-                x: Math.random() * 50,
-                y: Math.random() * 50,
-                repeat: -1, //indefinitely
-                ease: "none",
-                duration: 4,
-            });
-        });
+  useLayoutEffect(() => {
+    let isAnimating = true;
 
-        return () => ctx.revert();
-    }, []);
-    
+    const animateDot = () => {
+      if (!isAnimating) return;
+
+      // Random values for animation
+      const randomX = Math.random() * 100;
+      const randomY = Math.random() * 100;
+
+      const animation = gsap.to(dotRef.current, {
+        x: randomX, // Random X position
+        y: randomY, // Random Y position
+        ease: "none", // Easing function for smooth motion
+        duration: 4, // Animation duration in seconds
+        onComplete: () => {
+          if (isAnimating) {
+            animation.kill(); // Kill the current animation
+            animateDot(); // Trigger a new animation when the current one completes
+          }
+        },
+      });
+    };
+
+    // Start the initial animation
+    animateDot();
+
+    // Cleanup function to stop the animation
+    return () => {
+      isAnimating = false;
+    };
+  }, []);
+
   const dotStyle = {
     left: `${x}%`,
     top: `${y}%`,
   };
 
-  return (<div ref={dotRef} className="dot" style={dotStyle}/>)
+  return <div ref={dotRef} className="dot" style={dotStyle}></div>;
 }
 
 export default Dot;
