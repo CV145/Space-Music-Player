fcad158a65e0466d1f4af3314dc315b6b0867e17
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2023-11-04 16:07:14 -0500

    Added simple flow anim to dots

--- space-player/src/components/Dot.js
+++ space-player/src/components/Dot.js
@@ -1,14 +1,32 @@
 // Dot.js
-import React from 'react';
+import React, {useEffect, useLayoutEffect, useRef} from 'react';
 import '../styles/Dot.css'; // Import the CSS for styling
+import gsap from 'gsap';
 
 function Dot({ x, y }) {
+    const dotRef = useRef();
+
+    //Fires after all DOM mutations
+    useLayoutEffect(() => {
+        let ctx = gsap.context(() => {
+            gsap.to(dotRef.current, {
+                x: Math.random() * 50,
+                y: Math.random() * 50,
+                repeat: -1, //indefinitely
+                ease: "none",
+                duration: 4,
+            });
+        });
+
+        return () => ctx.revert();
+    }, []);
+    
   const dotStyle = {
     left: `${x}%`,
     top: `${y}%`,
   };
 
-  return <div className="dot" style={dotStyle}></div>;
+  return (<div ref={dotRef} className="dot" style={dotStyle}/>)
 }
 
 export default Dot;
